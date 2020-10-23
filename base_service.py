f101ab6c8937f6add9f97632952f62648ba87a64
Blazar <805781038@qq.com>
2020-10-23 02:40:15 -0700

    add more entity related to block

--- base_service.py
+++ base_service.py
@@ -10,3 +10,87 @@ def json_to_attestation_array(json):
 		res.append(item)
 	return res
 
+
+# Parse raw json from prysm api to block array, and extract the attached data like deposit, exiting, slashing attesters and slashing proposers
+def json_to_block_array(json):
+	res = []
+	for each in json['blockContainers']:
+		block_root = each['blockRoot']
+		block_outter = each['block']
+		sign = block_outter['signature']
+		block_inner = block['block']
+		slot = block_inner['slot']
+		proposer_index = block_inner['proposerIndex']
+		parent_root = block_inner['parent_root']
+		state_root = block_inner['state_root']
+		randao_reveal = block_inner['body']['randao_reveal']
+		dpst_root = block_inner['body']['eth1data']['depositRoot']
+		dpst_count = block_inner['body']['eth1data']['depositCount']
+		block_hash = block_inner['body']['eth1data']['blockHash']
+		grff = block_inner['graffiti']
+		has_deposit = False
+		deposits = []
+		has_exit = False
+		exits = []
+		has_slashinga = False
+		slashingas = []
+		has_slashingp = False
+		slashingps = []
+		if block_inner['body']['deposits']:
+			has_deposit = True
+			for deposit in block_inner['body']['deposits']:
+				proof = deposit['proof']
+				pub_key = deposit['data']['publicKey']
+				withdraw_cred = deposit['data']['withdrawCredentials']
+				amount = deposit['data']['amount']
+				dpst_sign = deposit['data']['signature']
+				deposits.append(deposit(slot, block_root, proof, pub_key, withdraw_cred, amount, dpst_sign)
+		if block_inner['body']['voluntaryExits']:
+			has_exit = True
+			for exit in block['inner']['voluntaryExits']:
+				exit_epoch = exit['exit']['epoch']
+				exit_index = exit['exit']['validatorIndex']
+				exit_sign = exit['signature']
+				exits.append(exit(slot, block_root, exit_epoch, exit_index, exit_sign)
+		if block_inner['body']['proposerSlashings']:
+			has_slashingp = True
+			for slashingp in block['body']['proposerSlashings']:
+				slot1 = slashingp['header1']['header']['slot']
+				index1 = slashingp['header1']['header']['proposerIndex']
+				parent_root1 = slashingp['header1']['header']['parentRoot']
+				state_root1 = slashingp['header1']['header']['stateRoot']
+				body_root1 = slashingp['header1']['header']['bodyRoot']
+				sign1 = slashingsp['header1']['signature']
+				slot2 = slashingp['header2']['header']['slot']
+				index2 = slashingp['header2']['header']['proposerIndex']
+				parent_root2 = slashingp['header2']['header']['parentRoot']
+				state_root2 = slashingp['header2']['header']['stateRoot']
+				body_root2 = slashingp['header2']['header']['bodyRoot']
+				sign2 = slashingsp['header2']['signature']
+				slashingps.append(slashingp(slot, block_root, slot1, index1, parent_root1, state_root1, body_root1, sign1, slot2, index2, parent_root2, state_root2, body_root2, sign2)
+		if block_inner['body']['attesterSlashings']:
+			has_slashinga = True
+			for slashinga in block['body']['attesterSlashings']:
+				indice1 = slashinga['attestation1']['attestingIndices']
+				slot1 = slashinga['attestation1']['data']['slot']
+				cmt_index1 = slashinga['attestation1']['data']['committeeIndex']
+				block_root1 = slashinga['attestation1']['data']['beaconBlockRoot']
+				src_epoch1 = slashinga['attestation1']['data']['source']['epoch']
+				src_root1 = slashinga['attestation1']['data']['source']['root']
+				trgt_epoch1 = slashinga['attestation1']['data']['target']['epoch']
+				trgt_root1 = slashinga['attestation1']['data']['target']['root']
+				sign1 = slashing['attestation1']['signature']
+				indice2 = slashinga['attestation2']['attestingIndices']
+				slot2 = slashinga['attestation2']['data']['slot']
+				cmt_index2 = slashinga['attestation2']['data']['committeeIndex']
+				block_root2 = slashinga['attestation2']['data']['beaconBlockRoot']
+				src_epoch2 = slashinga['attestation2']['data']['source']['epoch']
+				src_root2 = slashinga['attestation2']['data']['source']['root']
+				trgt_epoch2 = slashinga['attestation2']['data']['target']['epoch']
+				trgt_root2 = slashinga['attestation2']['data']['target']['root']
+				sign2 = slashing['attestation2']['signature']
+				slashinga.append(slashinga(slot, block_root, indice1, slot1, cmt_index1, block_root1, src_epoch1, src_root1, a1_trgt_ecoch, trgt_root1, sign1, indice2, slot2, cmt_index2, block_root2, src_epoch2, src_root2, a2_trgt_ecoch, trgt_root2, sign2)
+		b = block(slot, proposer_index, parent_root, state_root, randao_reveal, grff,sign, block_root, dpst_root, dpst_count, block_hash, has_dpst, has_exit, has_slashinga, has_slashingp)
+		res.append((b, deposits, exits, slashinga, slashingp))
+	return res
+		
--- entity.py
+++ entity.py
@@ -8,6 +8,8 @@ class attestation:
 	trgt_epoch = 0
 	trgt_root = ''
 	sign = ''
+
+
 	def __init__(self, aggr_bits, slot, cmt_index, block_root, src_epoch, src_block, trgt_epoch, trgt_block, sign):
 		self.aggr_bits = aggr_bits
 		self.slot = slot
@@ -18,6 +20,8 @@ class attestation:
 		self.trgt_epoch = trgt_epoch
 		self.trgt_block = trgt_block
 		self.sign = sign
+
+
 	def __repr__(self):
 		return 'aggr_bits={}, slot={}, cmt_index={}, block_root={}, src_epoch={}, src_root={}, trgt_epoch={}, trgt_root={}, sign={}'.format(self.aggr_bits, self.slot, self.cmt_index, self.block_root, self.src_epoch, self.src_block, self.trgt_epoch, self.trgt_block, self.sign)
 
@@ -28,94 +32,171 @@ class block:
 	parent_root = 0
 	state_root = 0
 	randao_reveal = ''
-	dpst_root = ''
-	dpst_count = 0
-	block_hash = ''
 	grff = ''
 	sign = ''
 	block_root = ''
-	def __init__(self, slot, proposer_index, parent_root, state_root, randao_reveal, dpst_root, dpst_count, block_hash, grff,sign, block_root):
+	dpst_root = ''
+	dpst_count = 0
+	block_hash = ''
+	has_dpst = False
+	has_exit = False
+	has_slashingp = False
+	has_slashinga = False
+
+
+	def __init__(self, slot, proposer_index, parent_root, state_root, randao_reveal, grff, sign, block_root, dpst_root, dpst_count, block_hash, has_dpst, has_exit, has_slashinga, has_slashingp):
 		self.slot = slot
 		self.proposer_index = proposer_index
 		self.parent_root = parent_root
 		self.state_root =state_root
 		self.randao_reveal = randao_reveal
-		self.dpst_root = dpst_root
-		self.dpst_count = dpst_count
-		self.block_hash = block_hash
 		self.grff = grff
 		self.sign = sign
 		self.block_root = block_root
+		self.dpst_root = dpst_root
+		self.dpst_count = dpst_count
+		self.block_hash = block_hash
+		self.has_dpst = has_dpst
+		self.has_exit = has_exit
+		self.has_slashinga = has_slashinga
+		self.has_slashingp = has_slashingp
 
 
-class slashing_a:
+	def __repr__(self):
+		return 'slot={},proposer_index={},parent_root={},state_root={},randao_reveal={},grff={}, sign={}, block_root={},dpst_root={},dpst_count={},block_hash={},has_dpst={},has_exit={},has_slashinga={},has_slashingp={}'.format(self.slot, self.proposer_index, self.parent_root, self.state_root, self.randao_reveal, self.grff, self.sign, self.block_root, self.dpst_root, self.dpst_count, self.block_hash, self.has_dpst, self.has_exit, self.has_slashinga, self.has_slashingp)
+
+
+class deposit:
 	slot = 0
-	a1_indice = 0
-	a1_slot = 0
-	a1_cmt_index = 0
-	a1_block_root = ''
-	a1_src_epoch = 0
-	a1_src_root = ''
-	a1_trgt_epoch = 0
-	a1_trgt_root = ''
-	a1_sign = ''
-	a2_indice = 0
-	a2_slot = 0
-	a2_cmt_index = 0
-	a2_block_root = ''
-	a2_src_epoch = 0
-	a2_src_root = ''
-	a2_trgt_epoch = 0
-	a2_trgt_root = ''
-	a2_sign = ''
-	def __init__(self, slot, a1_indice, a1_slot, a1_cmt_index, a1_block_root, a1_src_epoch, a1_src_root, a1_trgt_ecoch, a1_trgt_root, a1_sign, a2_indice, a2_slot, a2_cmt_index, a2_block_root, a2_src_epoch, a2_src_root, a2_trgt_ecoch, a2_trgt_root, a2_sign):
-			self.slot = slot
-			self.a1_indice = a1_indice
-			self.a1_slot = a1_slot
-			self.a1_cmt_index = a1_cmt_index
-			self.a1_block_root = a1_block_root
-			self.a1_src_epoch = a1_src_epoch
-			self.a1_src_root = a1_src_root
-			self.a1_trgt_epoch = a1_trgt_epoch
-			self.a1_trgt_root = a1_trgt_root
-			self.a1_sign = a1_sign
-			self.a2_indice = a2_indice
-			self.a2_slot = a2_slot
-			self.a2_cmt_index = a2_cmt_index
-			self.a2_block_root = a2_block_root
-			self.a2_src_epoch = a2_src_epoch
-			self.a2_src_root = a2_src_root
-			self.a2_trgt_epoch = a2_trgt_epoch
-			self.a2_trgt_root = a2_trgt_root
-			self.a2_sign = a2_sign
+	block_root = ''
+	proof = ''
+	pub_key = ''
+	withdraw_cred = ''
+	amount = 0
+	sign = ''
 
 
-class slashing_p:
-	slot = 0
-	validator_index = 0
-	h1_slot = 0
-	h1_parent_root = ''
-	h1_state_root = ''
-	h1_body_root = ''
-	h1_sign = ''
-	h2_slot = 0
-	h2_parent_root = ''
-	h2_state_root = ''
-	h2_body_root = ''
-	h2_sign = ''
-	def __init__(self, slot, validator_index, h1_slot, h1_parent_root, h1_state_root, h1_body_root, h1_sign, h2_slot, h2_parent_root, h2_state_root, h2_body_root, h2_sign):
+	def __init__(self, slot, block_root, proof, pub_key, withdraw_cred, amount, sign):
 		self.slot = slot
+		self.block_root = block_root
+		self.proof = proof
+		self.pub_key = pub_key
+		self.withdraw_cred = withdraw_cred
+		self.amount = amount
+		self.sign = sign
+
+
+	def __repr__(self):
+		return 'slot={}, block_root={},proof={},pub_key={},withdraw_cred={},amount={},sign={}'.format(self.slot, self.block_root, self.proof, self.pub_key, self.withdraw_cred, self.amount, self.sign)
+	
+
+class exit:
+	slot = 0
+	block_root = ''
+	exit_epoch = 0
+	validator_index = 0
+	sign = ''
+
+
+	def __init__(self, slot, block_root, exit_epoch, validator_index, sign):
+		self.slot = slot
+		self.block_root = block_root
+		self.exit_epoch = exit_epoch
 		self.validator_index = validator_index
-		self.h1_slot = h1_slot
-		self.h1_parent_root = h1_parent_root
-		self.h1_state_root = h1_state_root
-		self.h1_body_root = h1_body_root
-		self.h1_sign = h1_sign
-		self.h2_slot = h2_slot
-		self.h2_parent_root = h2_parent_root
-		self.h2_state_root = h2_state_root
-		self.h2_body_root = h2_body_root
-		self.h2_sign = h2_sign
+		self.sign = sign
+
+
+	def __repr__(self):
+		return 'slot={}, block_root={},exit_epoch={},validator_index={},sign={}'.format(self.slot, self.block_root, self.exit_epoch, self.validator_index, self.sign)
+
+
+class slashinga:
+	slot = 0
+	block_root = ''
+	indice1 = 0
+	slot1 = 0
+	cmt_index1 = 0
+	block_root1 = ''
+	src_epoch1 = 0
+	src_root1 = ''
+	trgt_epoch1 = 0
+	trgt_root1 = ''
+	sign1 = ''
+	indice2 = 0
+	slot2 = 0
+	cmt_index2 = 0
+	block_root2 = ''
+	src_epoch2 = 0
+	src_root2 = ''
+	trgt_epoch2 = 0
+	trgt_root2 = ''
+	sign2 = ''
+
+
+	def __init__(self, slot, block_root, indice1, slot1, cmt_index1, block_root1, src_epoch1, src_root1, a1_trgt_ecoch, trgt_root1, sign1, indice2, slot2, cmt_index2, block_root2, src_epoch2, src_root2, a2_trgt_ecoch, trgt_root2, sign2):
+		self.slot = slot
+		self.block_root = block_root
+		self.indice1 = indice1
+		self.slot1 = slot1
+		self.cmt_index1 = cmt_index1
+		self.block_root1 = block_root1
+		self.src_epoch1 = src_epoch1
+		self.src_root1 = src_root1
+		self.trgt_epoch1 = trgt_epoch1
+		self.trgt_root1 = trgt_root1
+		self.sign1 = sign1
+		self.indice2 = indice2
+		self.slot2 = slot2
+		self.cmt_index2 = cmt_index2
+		self.block_root2 = block_root2
+		self.src_epoch2 = src_epoch2
+		self.src_root2 = src_root2
+		self.trgt_epoch2 = trgt_epoch2
+		self.trgt_root2 = trgt_root2
+		self.sign2 = sign2
+
+
+	def __repr__(self):
+		return 'slot={},block_root={},indice1={},slot1={}, cmt_index1={},block_root1={},src_epoch1={},src_root1={},trgt_epoch1={},trgt_root1={},sign1={},indice2={},slot2={}, cmt_index2={},block_root2={},src_epoch2={},src_root2={},trgt_epoch2={},trgt_root2={},sign2={}'.format(self.slot,self.block_root,self.indice1,self.slot1,self.cmt_index1,self.block_root1,self.src_epoch1,self.src_root1,self.trgt_epoch1,self.sign1,self.indice2,self.slot2,self.cmt_index2,self.block_root2,self.src_epoch2,self.src_root2,
+self.trgt_epoch2,self.sign2)
+
+
+class slashingp:
+	slot = 0
+	block_root = ''
+	slot1 = 0
+	index1 = 0
+	parent_root1 = ''
+	state_root1 = ''
+	body_root1 = ''
+	sign1 = ''
+	slot2 = 0
+	index2 = 0
+	parent_root2 = ''
+	state_root2 = ''
+	body_root2 = ''
+	sign2 = ''
+
+
+	def __init__(self, slot, block_root, slot1, index1, parent_root1, state_root1, body_root1, sign1, slot2, index2, parent_root2, state_root2, body_root2, sign2):
+		self.slot = slot
+		self.block_root = block_root
+		self.slot1 = slot1
+		self.index1 = index1
+		self.parent_root1 = parent_root1
+		self.state_root1 = state_root1
+		self.body_root1 = body_root1
+		self.sign1 = sign1
+		self.slot2 = slot2
+		self.index2 = index2
+		self.parent_root2 = parent_root2
+		self.state_root2 = state_root2
+		self.body_root2 = body_root2
+		self.sign2 = sign2
+
+
+	def __repr__(self):
+		return 'slot={},block_root={},slot1={},index1={},parent_root1={},state_root1={},body_root1={},sign1={},slot2={},index2={},parent_root2={},state_root2={},body_root2={},sign2={}'.format(self.slot, self.block_root,self.slot1, self.index1, self.parent_root1, self.state_root1, self.body_root1, self.sign1, self.slot2, self.index2,self.parent_root2, self.state_root2, self.body_root2, self.sign2)
 
 	
 	
