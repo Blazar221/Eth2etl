0d8fc25db05b0a353c504e0f6fcf519221235695
Blazar <805781038@qq.com>
2021-01-18 11:15:39 +0800

    change aggregation_bits;change 243 location

--- mapper/attestation_mapper.py
+++ mapper/attestation_mapper.py
@@ -4,8 +4,9 @@ from base64 import standard_b64decode
 
 def json_item_to_attestation(json_item):
     attestation = Attestation()
-
-    attestation.aggregation_bits = hex_to_aggregation_bits(json_item['aggregationBits'])
+# Keep the raw aggregation_bits here, because the converting it to 01 combination is not verified yet. 
+#   attestation.aggregation_bits = hex_to_aggregation_bits(json_item['aggregationBits'])
+	attestation.aggregation_bits = json_item['aggregationBits']	
 
     raw_data = json_item['data']
 
